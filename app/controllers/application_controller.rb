9f68bd8a0f00466ba599d0ce40e86111852e6512
github username <github email address>
2021-06-17 14:50:28 -0500

    built out application controller and created methods for auth using jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  def secret_key
+    "secret"
+  end
+
+  #given a payload {user_id: ?} => token
+  def encode(payload)
+    JWT.encode(payload, secret_key, 'HS256')
+  end
+
+  def decode(token)
+    JWT.decode(token, "secret", true, {algorithm: "HS256"})[0]
+  end
 end
