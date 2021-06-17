731862d0a3b661a667a546f16682313670be9897
github username <github email address>
2021-06-17 14:56:27 -0500

    built out login and token authenticate methods within application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,23 @@ class ApplicationController < ActionController::Base
   def decode(token)
     JWT.decode(token, "secret", true, {algorithm: "HS256"})[0]
   end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      payload = {user_id: user.id}
+      token = encode(payload)
+      render :json => {user: user, token: token}
+    else
+      render json: {error: "User not found"}
+    end
+
+  def token_authenticate
+
+    token = request.headers["Authenticate"]
+    user = User.find(decode(token)["user_id"])
+
+    render json: user
+  end
 end
